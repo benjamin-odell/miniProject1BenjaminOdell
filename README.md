ac21043716190da98bae526c47ef1899114dabc0
Benjamin Odell <benjamin.m.odell@proton.me>
2026-01-29 17:04:09 -0600

    Saved plots to file. Updated .gitignore to ignore plots folder, and updated readme for source used during saving.

--- README.md
+++ README.md
@@ -57,7 +57,7 @@ This project is licensed under the [NAME HERE] License - see the LICENSE.md file
 ## Acknowledgments
  
 Inspiration, code snippets, etc.
-* [awesome-readme](https://github.com/matiassingers/awesome-readme)
+* [Stack Overflow: Save matplotlib file to a directory](https://stackoverflow.com/questions/11373610/save-matplotlib-file-to-a-directory#:~:text=def,raise)
 * [PurpleBooth](https://gist.github.com/PurpleBooth/109311bb0361f32d87a2)
 * [dbader](https://github.com/dbader/readme-template)
 * [zenorocha](https://gist.github.com/zenorocha/4526327)
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pprint
 import stock_tickers
 import numpy as np
 import matplotlib.pyplot as plt
+from os import makedirs, path
 
 #Get the tickers for my chosen stocks
 stocks = yf.Tickers(stock_tickers.stocks())
@@ -18,9 +19,13 @@ for ticker in stocks.tickers:
     data = yf.Ticker(ticker).history(period="10d")['Close']
     closing[ticker] = np.array(data)
 
+#checks if there is a plots dir if not create one
+if not path.isdir('plots'):
+    makedirs('plots')
+
 #loop through each ticker and output image with matplot lib
 for stock in closing:
     plt.plot(closing[stock])
-    plt.show()
+    plt.savefig(path.join(path.dirname(__file__), 'plots', stock + '.png'))
 #prints out data
 pprint.pprint(closing)
